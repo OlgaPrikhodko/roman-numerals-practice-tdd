99610e60a7e69decd0a47328ec75e40236db188b
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-01-10 08:58:48 +0200

    add test cases for 309, 527 and 789

--- src/roman_numerals.test.js
+++ src/roman_numerals.test.js
@@ -27,3 +27,21 @@ test("convert 49 to XLIX", () => {
 test("convert 99 to XCIX", () => {
   expect(numToRoman(99)).toBe("XCIX");
 });
+
+test("convert 309 to CCCIX", () => {
+  expect(numToRoman(309)).toBe("CCCIX");
+});
+
+test("convert 527 to DXXVII", () => {
+  expect(numToRoman(527)).toBe("DXXVII");
+});
+
+test("convert 789 to DCCLXXXIX", () => {
+  expect(numToRoman(789)).toBe("DCCLXXXIX");
+});
+
+//     >>> roman(1888)
+//     'MDCCCLXXXVIII'
+//     >>> roman(1999)
+//     'MCMXCIX'
+//     >>> roman(2001)
